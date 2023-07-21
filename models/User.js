2b6177551ed7416670eee160787bcf9046cf96e2
erinbedri <erinbedri@gmail.com>
2023-07-21 16:05:54 +0200

    created user schema

---
+++ models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide name"],
+        minLength: 2,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide email"],
+        match: [
+            /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+            "Please provide valid email",
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide password"],
+        minLength: 6,
+        maxLength: 12,
+    },
+});
+
+module.exports = mongoose.model("User", UserSchema);
