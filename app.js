51304b9d2963e72be519c597459222a93a5865d1
erinbedri <erinbedri@gmail.com>
2023-07-27 14:39:13 +0200

    authentication middleware

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const app = express();
 // connectDB
 const connectDB = require("./db/connect");
 
+// authenticate user
+const authenticateUser = require("./middleware/authentication");
+
 // routers
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 
-// error handler
+// error handlers
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
@@ -19,7 +22,7 @@ app.use(express.json());
 
 // routes
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
