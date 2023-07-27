f2fc9c7a667a9e4d1dcf9f2185670bba5d0cdf60
erinbedri <erinbedri@gmail.com>
2023-07-27 13:21:58 +0200

    create jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const { BadRequestError } = require("../errors");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
+    const token = jwt.sign({ userId: user._id, name: user.name }, "JWTSecret", { expiresIn: "30d" });
 
-    res.status(StatusCodes.CREATED).json({ user });
+    res.status(StatusCodes.CREATED).json({ name: user.name, token });
 };
 
 const login = (req, res) => {
