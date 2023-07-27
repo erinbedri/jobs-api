aceb734818a14f5b1d54bc15c7ff1afbe0e6156d
erinbedri <erinbedri@gmail.com>
2023-07-27 13:30:49 +0200

    createJWT as schema method

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,11 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
-    const token = jwt.sign({ userId: user._id, name: user.name }, "JWTSecret", { expiresIn: "30d" });
+    const token = user.createJWT();
 
-    res.status(StatusCodes.CREATED).json({ name: user.name, token });
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = (req, res) => {
