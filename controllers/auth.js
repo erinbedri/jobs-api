f4f22aa3abe20cdd35f878e55b1f651d44acd558
erinbedri <erinbedri@gmail.com>
2023-07-27 13:13:11 +0200

    extracted hash password func as middleware to user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,11 @@
-const register = (req, res) => {
-    res.send("register user");
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+const { BadRequestError } = require("../errors");
+
+const register = async (req, res) => {
+    const user = await User.create({ ...req.body });
+
+    res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = (req, res) => {
